cc51a0f7b3b3fdb9b8be7763b582f911ea76f3ff
Jolley <108022961+jolleyDesign@users.noreply.github.com>
2022-06-27 13:10:04 -0600

    Add files via upload

--- colorGen.py
+++ colorGen.py
@@ -9,6 +9,27 @@ redValue = random.randint(0,256)
 greenValue = random.randint(0,256)
 blueValue = random.randint(0,256)
 
+# defining newButton function
+## definitely a simpler way to incorporate this, just lazy right now
+
+def newColor():
+    redValue = random.randint(0,256)
+    greenValue = random.randint(0,256)
+    blueValue = random.randint(0,256)
+    hexVal = "%02x%02x%02x" % (redValue, greenValue, blueValue)
+    hexVal = "#" + hexVal.upper()
+    print(hexVal)
+
+    window.title(hexVal)
+    window.geometry("500x500")
+    window.config(background=hexVal)
+    
+    entryLabel = Entry(window, bg="white", fg="black")
+    entryLabel.place(relx=.5, rely=.5, anchor=CENTER)
+    entryLabel.insert(0, hexVal)
+    entryLabel.configure(state="readonly", justify="center")
+
+
 # converting RGB to HEX
 
 hexVal = "%02x%02x%02x" % (redValue, greenValue, blueValue)
@@ -20,7 +41,12 @@ window.title(hexVal)
 window.geometry("500x500")
 window.config(background=hexVal)
 
-label = ttk.Label(window, text=hexVal, background="white", foreground="black")
-label.place(relx=.5, rely=.5, anchor=CENTER)
+entryLabel = Entry(window, bg="white", fg="black")
+entryLabel.place(relx=.5, rely=.5, anchor=CENTER)
+entryLabel.insert(0, hexVal)
+entryLabel.configure(state="readonly", justify="center")
+
+newButton = Button(window, text="New color", background="white", command=newColor)
+newButton.place(relx=.5, rely=.8, anchor=CENTER)
 
 window.mainloop()
